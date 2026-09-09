452b04b7116df75caced6c0bd94687b052a7da52
sanghyeonJ <jsh080000@gmail.com>
2026-09-09 18:24:32 +0900

    초기 세팅

---
+++ src/main/java/com/muckzi/muckziback/MuckziBackApplication.java
@@ -0,0 +1,13 @@
+package com.muckzi.muckziback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuckziBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuckziBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muckzi/muckziback/MuckziBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muckzi.muckziback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuckziBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
