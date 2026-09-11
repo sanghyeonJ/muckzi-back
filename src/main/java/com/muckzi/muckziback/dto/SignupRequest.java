0b81f48daf9da1ad188e4085b86d166858ac4d87
sanghyeonJ <jsh080000@gmail.com>
2026-09-12 00:25:43 +0900

    fix: 닉네임 공백 허용

--- src/main/java/com/muckzi/muckziback/dto/SignupRequest.java
+++ src/main/java/com/muckzi/muckziback/dto/SignupRequest.java
@@ -29,7 +29,7 @@ public class SignupRequest {
     @NotBlank(message = "닉네임을 입력해주세요.")
     @Size(min = 2, max = 10, message = "닉네임은 2~10자로 입력해주세요.")
     @Pattern(
-            regexp = "^[가-힣a-zA-Z0-9]+$",
+            regexp = "^[가-힣a-zA-Z0-9 ]+$",
             message = "닉네임은 한글, 영문, 숫자만 사용할 수 있습니다."
     )
     private String nickname;
